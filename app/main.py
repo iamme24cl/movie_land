cbf574cd02599610ce3818030fc85e94e675477c
Chandra Lama <iamme24cl@gmail.com>
2024-05-03 13:05:36 -0700

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from resolver import random_movies, random_genres_movies
 from recommender import movie_based_recommendation, user_based_recommendation
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
